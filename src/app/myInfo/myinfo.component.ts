c307528284e85135d2a02acc7ed736c742b36037
rmkw <creditozona1@gmail.com>
2024-03-20 10:08:02 -0600

    start:fire:

---
+++ src/app/myInfo/myinfo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-myInfo',
+  templateUrl: './myinfo.component.html',
+  styleUrls: ['./myinfo.component.css'],
+})
+export class appMyInfo {}
